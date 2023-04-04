9e1a061be7f6e807a38df2387dd75fd5a62e699e
zchips <zchips@gmail.com>
2023-04-04 12:24:36 -0600

    trying again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +10,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacter] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://swapi.dev/api/people/")
+    .then(resp => {
+      setCharacter(resp.data)
+    })
+    .catch(err => {console.log(err)})
+  }, [])
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters && <Character info ={character} key ={character.name}></CharacterCard>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,4 @@
 // Write your Character component here
+
+import React, {useState} from "react"
+import styled from 'styled-components'
